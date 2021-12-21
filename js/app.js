c6aea410332ebce0f85e9b889edbd5987a9428cf
nazdak504 <aguiluz.matt@gmail.com>
2021-12-20 22:30:56 -0600

    initial setup complete; pseudocoded ideas for UI/UX in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 console.log('===========mfa porfolio==========')
+
+
+// contact info: copy text on click
+// home link appears after a card is clicked and navigated into
+// main-container transitions into show page for each card
+// background image is visual reference for each show page
+// page transitions on each click
+// https://www.awwwards.com/page-transitions-creative-examples-resources-and-some-tips.html
